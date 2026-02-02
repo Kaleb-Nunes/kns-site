5e16dc9f559a66798273cdbecbdd1b579c6b9c49
Kaleb Nunes dos Santos <kalebdossantos59924@gmail.com>
2026-02-02 07:00:10 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx' // ou './App.js' se você manteve .js
+import App from './App.js' // ou './App.js' se você manteve .js
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+
 )
